6c8601fc44829658d7c35aa0c2f6350e7fbf1d5f
Timothy Hitchener <thitchener@pivotal.io>
2020-06-01 16:02:03 -0400

    implement buildpack that provides unicorn start cmd
    - uses packit lib
    - bootstrapped with latest github-config
    
    Co-authored-by: Arjun Sreedharan <asreedharan@vmware.com>

---
+++ detect_test.go
@@ -0,0 +1,141 @@
+package main_test
+
+import (
+	"errors"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"io/ioutil"
+
+	"github.com/paketo-buildpacks/packit"
+	main "github.com/paketo-community/unicorn"
+	"github.com/paketo-community/unicorn/fakes"
+	"github.com/sclevine/spec"
+
+	. "github.com/onsi/gomega"
+)
+
+func testDetect(t *testing.T, context spec.G, it spec.S) {
+	var (
+		Expect = NewWithT(t).Expect
+
+		workingDir    string
+		gemfileParser *fakes.Parser
+		detect        packit.DetectFunc
+	)
+
+	it.Before(func() {
+		var err error
+		workingDir, err = ioutil.TempDir("", "working-dir")
+		Expect(err).NotTo(HaveOccurred())
+
+		err = ioutil.WriteFile(filepath.Join(workingDir, "Gemfile"), []byte{}, 0644)
+		Expect(err).NotTo(HaveOccurred())
+
+		err = ioutil.WriteFile(filepath.Join(workingDir, "config.ru"), []byte{}, 0644)
+		Expect(err).NotTo(HaveOccurred())
+
+		gemfileParser = &fakes.Parser{}
+
+		detect = main.Detect(gemfileParser)
+	})
+
+	it.After(func() {
+		Expect(os.RemoveAll(workingDir)).To(Succeed())
+	})
+
+	context("when the Gemfile lists unicorn", func() {
+		it.Before(func() {
+			gemfileParser.ParseCall.Returns.HasUnicorn = true
+		})
+		it("detects", func() {
+			result, err := detect(packit.DetectContext{
+				WorkingDir: workingDir,
+			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(result.Plan).To(Equal(packit.BuildPlan{
+				Provides: []packit.BuildPlanProvision{},
+				Requires: []packit.BuildPlanRequirement{
+					{
+						Name: "gems",
+						Metadata: main.BuildPlanMetadata{
+							Launch: true,
+						},
+					},
+					{
+						Name: "bundler",
+						Metadata: main.BuildPlanMetadata{
+							Launch: true,
+						},
+					},
+					{
+						Name: "mri",
+						Metadata: main.BuildPlanMetadata{
+							Launch: true,
+						},
+					},
+				},
+			}))
+		})
+	})
+
+	context("when the Gemfile does not list unicorn", func() {
+		it.Before(func() {
+			gemfileParser.ParseCall.Returns.HasUnicorn = false
+		})
+
+		it("detect should fail with error", func() {
+			_, err := detect(packit.DetectContext{
+				WorkingDir: workingDir,
+			})
+			Expect(err).To(MatchError(packit.Fail))
+		})
+	})
+
+	context("when the workingDir does not have a config.ru", func() {
+		it.Before(func() {
+			gemfileParser.ParseCall.Returns.HasUnicorn = true
+			Expect(os.Remove(filepath.Join(workingDir, "config.ru"))).To(Succeed())
+		})
+
+		it("detect should fail with error", func() {
+			_, err := detect(packit.DetectContext{
+				WorkingDir: workingDir,
+			})
+			Expect(err).To(MatchError(packit.Fail))
+		})
+	})
+
+	context("failure cases", func() {
+		context("when the gemfile parser fails", func() {
+			it.Before(func() {
+				gemfileParser.ParseCall.Returns.Err = errors.New("some-error")
+			})
+
+			it("returns an error", func() {
+				_, err := detect(packit.DetectContext{
+					WorkingDir: workingDir,
+				})
+				Expect(err).To(MatchError("failed to parse Gemfile: some-error"))
+			})
+		})
+
+		context("when unable to stat config.ru", func() {
+			it.Before(func() {
+				Expect(os.Chmod(workingDir, 0000)).To(Succeed())
+			})
+
+			it.After(func() {
+				Expect(os.Chmod(workingDir, os.ModePerm)).To(Succeed())
+			})
+
+			it("returns an error", func() {
+				_, err := detect(packit.DetectContext{
+					WorkingDir: workingDir,
+				})
+				Expect(err).To(MatchError(ContainSubstring("failed to stat config.ru")))
+			})
+		})
+	})
+}
