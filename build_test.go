5c04ba33048decbc7aa87dcfa1fec72d8e725d98
Sophie Wigmore <swigmore@vmware.com>
2020-11-13 10:26:49 -0500

    Removes deprecated BuildResult.Processes field

--- build_test.go
+++ build_test.go
@@ -70,10 +70,12 @@ func testBuild(t *testing.T, context spec.G, it spec.S) {
 				Entries: nil,
 			},
 			Layers: nil,
-			Processes: []packit.Process{
-				{
-					Type:    "web",
-					Command: `bundle exec unicorn --listen "${PORT:-8080}"`,
+			Launch: packit.LaunchMetadata{
+				Processes: []packit.Process{
+					{
+						Type:    "web",
+						Command: `bundle exec unicorn --listen "${PORT:-8080}"`,
+					},
 				},
 			},
 		}))
